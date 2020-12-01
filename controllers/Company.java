8c7926bffb9e7a01d89618eade41467831814e2b
MiguelR-o <m.b.rosario@hotmail.com>
2020-12-01 22:12:15 +0000

    Employee Interface added , command RC boiler plate completed , Company Interface updated.

--- controllers/Company.java
+++ controllers/Company.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import models.Employee;
+
 public interface Company {
     boolean hasCategory(String category);
     //searches if there is a key equal to Category inside Professionals Dic
 
-    boolean hasProfessionalInCategory(String name, String category);
+    boolean hasProfessional(String name, String category);
     //searches for the key Category in Professionals Dic
     // and then for the name key in the given Dic
 
@@ -12,8 +14,18 @@ public interface Company {
     //Don't know how to implement the hard code part of the rule set yet ,
     // but it should then search for the Permission inside the Category class/interface/Dic
 
-    void registerEmployee(int employeeID, String category, String permission, String name);
+    void registerEmployee(String category, String permission, String name);
     //Creates instance of one of the classes that implements/extends Employee based on its
     //category and adds the object to the Employees Dic in Company.
 
+    boolean hasClient(String nameClient);
+
+
+    void registerClient(String nameClient);
+
+    String getClientID(String nameClient);
+
+    int getEmployeeID(Employee employee);
+
+    Employee getProfessional(String name, String category);
 }
--- views/Cli.java
+++ views/Cli.java
@@ -17,21 +17,28 @@ public class Cli {
             String[] command = line.split(" ");
             switch (command[0]) {
                 case "RF":
-                    int employeeID = Integer.parseInt(command[1]);
-                    String category = command[2];
-                    String permission = command[3];
-                    String name = nameConcatenation(4,command);
+                    String category = command[1];
+                    String permission = command[2];
+                    String name = nameConcatenation(3,command);
                     if(!company.hasCategory(category)){
                         System.out.println("Categoria inexistente.");
                     }else if(company.validPermission(permission,category)){
                         System.out.println("Permissão inexistente.");
-                    }else if(company.hasProfessionalInCategory(name,category)){
+                    }else if(company.hasProfessional(name,category)){
                     System.out.println("Funcionário existente.");
                 }else{
-                        company.registerEmployee(employeeID,category,permission,name);
+                        company.registerEmployee(category,permission,name);
+                        System.out.println("Funcionário registado com o identificador " + company.getEmployeeID(company.getProfessional(name,category)));
                 }
                     break;
                 case "RC":
+                    String nameClient = command[1];
+                    if(company.hasClient(nameClient)){
+                        System.out.println("Cliente existente.");
+                    }else{
+                        company.registerClient(nameClient);
+                        System.out.println("Cliente registado com o identificador "+ company.getClientID(nameClient));
+                    }
                     break;
                 case "RI":
                     break;
