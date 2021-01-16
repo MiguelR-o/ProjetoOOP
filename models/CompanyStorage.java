9dbc643bb51a5857a1815723c1d966ac7601fa22
MiguelR-o <m.b.rosario@hotmail.com>
2021-01-16 21:08:26 +0000

    hasCategory in CompanyClass is done.

--- models/CompanyStorage.java
+++ models/CompanyStorage.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 public interface CompanyStorage<T>{
     // TODO complete the Interface if a new default method is found
     boolean hasCaseName(String name);
-
-     ArrayList<T> getKeys();
     // given a string it searchs the values for value.getName()
     // and returns True if name == value.getname()
+
+     ArrayList<T> getKeys();
+     //returns a ListArray with all the keys
+
 
 }
