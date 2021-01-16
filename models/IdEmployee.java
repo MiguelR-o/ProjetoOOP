51fae2dfa83920541349d65f9f5e0495ded49512
MiguelR-o <m.b.rosario@hotmail.com>
2021-01-16 20:42:08 +0000

    convertToID com switch cases adicionado a CompanyClass

--- models/IdEmployee.java
+++ models/IdEmployee.java
@@ -2,5 +2,19 @@ package models;
 
 public class IdEmployee implements ID {
     private int ID;
-    private static final String type;
+    //TODO make all types in the ID classes use type as static final !!!
+    private static final String type = "Employee";
+
+    public IdEmployee(int IdNumber){
+        this.ID = IdNumber;
+    }
+    @Override
+    public int getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String getType() {
+        return this.type;
+    }
 }
