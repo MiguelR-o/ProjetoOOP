22a7c3ceb35b693225b99c1ebc8d5763eb483963
MiguelR-o <m.b.rosario@hotmail.com>
2021-01-17 10:31:35 +0000

    Metodos necessarios para funcionar ate ao RL estao feitos.

--- models/Place.java
+++ models/Place.java
@@ -1,4 +1,17 @@
 package models;
 
 public class Place {
+    private String name;
+    private ID ID;
+    public Place(String name , ID placeID){
+        this.name = name;
+        this.ID = placeID;
+    }
+
+    public ID getPlaceID(){
+        return this.ID;
+    }
+    public String getName() {
+        return this.name;
+    }
 }
